b785650ccd5748b2409a8433bc08c7fcc56f00e8
SahandM96 <SahandM96@hotmail.com>
2022-05-07 13:37:47 +0430

    publish on github

--- flutter_app/android/app/src/main/kotlin/com/sahandm96/flutter_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.sahandm96.flutter_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- flutter_app/android/app/src/main/kotlin/com/sahandm96/word_game/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.sahandm96.word_game
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
